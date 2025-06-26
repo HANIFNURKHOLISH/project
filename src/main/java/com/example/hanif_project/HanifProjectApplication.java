d0ff9fc1bf542105086bd11924c3a8d86c615468
hanif <>
2025-06-26 20:26:54 +0700

    hanif
    initialcomite

---
+++ src/main/java/com/example/hanif_project/HanifProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanif_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanifProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanifProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hanif_project/HanifProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanif_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanifProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
